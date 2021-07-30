e15123cf34c554555d53d46edb97f53cfbea0063
Adam Aslan <chillcoders@gmail.com>
2021-07-30 15:58:20 -0400

    next-css

--- dfl/pages/_app.js
+++ dfl/pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+// pages/_app.js
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
