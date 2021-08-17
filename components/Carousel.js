a823bb30e88153756c1c6674ec937b3994aa4957
Adam Aslan <chillcoders@gmail.com>
2021-08-17 17:11:55 -0400

    inline

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Carousel from "nuka-carousel";
-
+//https://kenwheeler.github.io/nuka-carousel/#/
 export default class extends React.Component {
   render() {
     return (
-      <Carousel>
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide1" />
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide2" />
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide3" />
+      <Carousel
+        slidesToShow={3}
+        cellAlign="center"
+        slideWidth={0.5}
+        cellSpacing={20}
+      >
+        <img src="https://lh3.googleusercontent.com/SpfSt4EadOjpRUZCfjEPu_bl5mCTyXq5skTyHKKqwcCt1hUzXHZ5ZxU9rPg_oY4e_tIZQQnZb4RD4FjBFzJTmMGKArLmctic3R1oD7GbBy_kXHazlVy1QdGWiN0ywz1GGenQRrCl2w=w2400" />
+        <img src="https://lh3.googleusercontent.com/uNvMIWcJl7Fn3vuEWe2k0VgJe7oAGLOEQ65Mw1UCOMqvmOIdsP2GJ2jd8bnt2dKDfUl-7WWOTFk8pozU74MTy0A5XOxLlhUL1ROAJtK0RH03Bbo-PC5Tpgk68Rre8naoqWytCgCunQ=w2400" />
+        <img src="https://lh3.googleusercontent.com/uucnoXh-7hhllptS6JzDoBL1_5Cy9u_kq3herLqXTN67FAE_mXn-YxMm5bw4JVZ52lg16FjCTJYBn_DPXy4Aqa0Hw2U4mYes_xKASLHZx70xQGWV3yh7vzSmamKyRGm3BrYiNRkPww=w2400" />
         <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide4" />
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide5" />
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide6" />
       </Carousel>
     );
   }
