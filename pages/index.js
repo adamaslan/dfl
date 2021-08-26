2645464fc0e4d8b3ff44be53251e52ce8e7f09ad
Adam <chillcoders@gmail.com>
2021-08-26 15:17:20 -0400

    card-stuff

--- pages/index.js
+++ pages/index.js
@@ -36,25 +36,27 @@ export default function Home() {
         <p> We miss Loic and Will!</p>
         <p> We miss Loic and Will!</p>
       </div>
-      <div className="wrapper">
-        <ContentCard
-          img="https://images.unsplash.com/photo-1536304929831-ee1ca9d44906?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-          title="The Everyday Salad"
-          description="Take your boring salads up a knotch. This recipe is perfect for lunch
+      <div className="smain3">
+        <div className="wrapper">
+          <ContentCard
+            img="https://images.unsplash.com/photo-1536304929831-ee1ca9d44906?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+            title="The Everyday Salad"
+            description="Take your boring salads up a knotch. This recipe is perfect for lunch
           and only contains 5 ingredients!"
-        />
+          />
 
-        <ContentCard
-          img="https://images.unsplash.com/photo-1476124369491-e7addf5db371?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-          title="Simple Risotto"
-          description="Fear Risotto no more! This simple recipe is perfect for family dinners."
-        />
+          <ContentCard
+            img="https://images.unsplash.com/photo-1476124369491-e7addf5db371?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+            title="Simple Risotto"
+            description="Fear Risotto no more! This simple recipe is perfect for family dinners."
+          />
 
-        <ContentCard
-          img="https://images.unsplash.com/photo-1529928520614-7c76e2d99740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-          title="Baked Cod with Vegetables"
-          description="Baked Cod with Vegetables. 30 minute meal!"
-        />
+          <ContentCard
+            img="https://images.unsplash.com/photo-1529928520614-7c76e2d99740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+            title="Baked Cod with Vegetables"
+            description="Baked Cod with Vegetables. 30 minute meal!"
+          />
+        </div>
       </div>
     </div>
   );
