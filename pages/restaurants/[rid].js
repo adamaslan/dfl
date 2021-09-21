0717ddee76e0adf1a8a7e76ae94c2d99602d8083
Adam <chillcoders@gmail.com>
2021-09-21 13:55:37 -0400

    ssr-pages

--- pages/restaurants/[rid].js
+++ pages/restaurants/[rid].js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Restaurant = ({ restaurantId }) => {
+const Restaurant = ({ restaurantId, result }) => {
+  console.dir(result);
   return (
     <div>
       <p>{restaurantId}</p>
@@ -12,12 +13,16 @@ export async function getServerSideProps(context) {
   const { params } = context;
   const { rid } = params;
   console.log(rid);
+  const result = await fetch(`http://localhost:3000/api/hello?id=${rid}`).then(
+    (R) => R.json()
+  );
 
   return {
     props: {
       restaurantId: rid,
+      result: result,
     },
   };
 }
 
-export default Restaurant
+export default Restaurant;
