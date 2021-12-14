c0f34e6a1e301668c992743c341299586aaba6c8
Adam <chillcoders@gmail.com>
2021-12-13 21:04:56 -0500

    fun

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,41 @@
-import Link from "next/link";
-import navStyles from "../styles/Nav.module.css";
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
 
-const Nav = () => {
+export default (props) => {
   return (
-    <nav className={navStyles.nav}>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
+    <Menu {...props}>
+      <a className="menu-item" href="/">
+        Home
+      </a>
+
+      <a className="menu-item" href="/about">
+        About
+      </a>
+
+      <a className="menu-item" href="/services">
+        Services
+      </a>
+
+      <a className="menu-item" href="/contact">
+        Contact us
+      </a>
+    </Menu>
   );
 };
 
-export default Nav;
+// const Nav = () => {
+//   return (
+//     <nav className={navStyles.nav}>
+//       <ul>
+//         <li>
+//           <Link href="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link href="/about">About</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default Nav;
