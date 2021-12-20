bb80d9f6dccd2eefb1b67c4b14bb166848650ed8
Adam <chillcoders@gmail.com>
2021-12-19 21:44:13 -0500

    vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.scss";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Layout.module.css";
-import Carousel from "../components/Carousel";
 import ContentCard from "../components/ContentCard";
 import Link from "next/link";
 
