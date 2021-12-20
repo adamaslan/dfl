ee7441f20600b8352ca9448923a44b30465bdabe
Adam <chillcoders@gmail.com>
2021-12-19 20:52:28 -0500

    class

--- components/ContentCard.js
+++ components/ContentCard.js
@@ -4,7 +4,7 @@ const ContentCard = (props) => {
   return (
     <div className="card">
       <div className="card__body">
-        <img src={props.img} class="card__image" />
+        <img src={props.img} className="card__image" />
         <h2 className="card__title">{props.title}</h2>
         <p className="card__description">{props.description}</p>
       </div>{" "}
