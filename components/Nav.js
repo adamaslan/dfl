a8457b83abe465056f2dbf19ecc24d4f044efa4e
Adam <chillcoders@gmail.com>
2021-12-23 12:02:25 -0500

    nav-ani

--- components/Nav.js
+++ components/Nav.js
@@ -95,6 +95,43 @@ const Hamburger = styled.div`
     margin-bottom: 4px;
     border-radius: 5px;
   }
+
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
+
+  &:focus {
+    outline: none;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ isOpen }) => (isOpen ? "#0D0C1D" : "#EFFFFA")};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+
+    :first-child {
+      transform: ${({ isOpen }) => (isOpen ? "rotate(45deg)" : "rotate(0)")};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ isOpen }) => (isOpen ? "0" : "1")};
+      transform: ${({ isOpen }) =>
+        isOpen ? "translateX(20px)" : "translateX(0)"};
+    }
+
+    :nth-child(3) {
+      transform: ${({ isOpen }) => (isOpen ? "rotate(-45deg)" : "rotate(0)")};
+    }
+  }
   @media (max-width: 768px) {
     display: flex;
   }
