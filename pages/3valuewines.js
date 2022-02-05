d8fb620cd3fc0e3dd13d384bc44dcfca1018edfd
Adam <chillcoders@gmail.com>
2022-02-04 21:49:46 -0500

    article

---
+++ pages/3valuewines.js
@@ -0,0 +1,23 @@
+<h1>3 Low Cost High Value Red Wines</h1>
+
+<p>
+If you are looking for wine that is low in cost and high in value, we have found 3 that fit the description perfectly.
+
+1. Santa Rita’s 120 Carménère
+
+While Carménère is not one of the most popular types of wine out there it certainly a solid bodied red that is the pride of Chile. What makes Santa Rita’s Carménère stand out from the croud is its uniquely pleasent mouthfeel characterized by a deep fruitiness. Is it sweet? Yes, but still elegant and with a strong nose full of red fruit and crisp spices.
+
+Especially recommended for the budding red wine enthusiast looking to diversify their palate, the 120 Carménère evokes the nostalgia for Chilean culture through its taste as well as the ‘120’ series name, which is titled after Doña Paula Jaraquemada who hid 120 Chilean revolutionaries during their war for independence from Spain.
+</p>
+<p>
+2) Fetzer Valley Oaks Cabernet Sauvignon
+
+With grapes grown in the valleys of Mendocino County, Fetzer’s Valley Oaks offers delightful black currant aromas of aromas of higher priced Cabernet Sauvignons at an extremely affordable cost. There is plenty to like about this wine with its rich body and smooth finish. Owned by Latin American wine giants Concho y Toro, Fetzer has created a lovely Cabernet Sauvignon in the foothills of Northern California.
+</p>
+
+
+3) Casillero del Diablo Reserva Cabernet Sauvignon
+<p>
+Described by the wine maker as “Medium bodied with silky tannins” the Casillero del Diablo Reserva is yet another solid bet for those looking for value at a modest price. Rich in cherry flavors, this Cabernet Sauvignon sitting beautifully in the glass with its dark red color foreshadows its great taste just by its appearance alone. Finding a solid bottle of red wine at good price is not always the easiest thing, but these three reds will not do you wrong. They may not be the best examples of their specific type of red but they are definitely worth a try.
+</p>
+
