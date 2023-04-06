ca834a2ce4a535ae41e3b439deed7d09b99f1d5f
Adam Aslan <adamaslan@Adams-MacBook-Air.local>
2023-04-05 21:44:57 -0400

    content-card-upgrade

---
+++ pages/loading.js
@@ -0,0 +1,28 @@
+import { Suspense } from 'react'
+
+export default function Loading() {
+   return <>
+   <style jsx>{`
+      .loader {
+        border: 16px solid #f3f3f3;
+        border-top: 16px solid #3498db;
+        border-radius: 50%;
+        width: 1200px;
+        height: 1200px;
+        animation: spin 2s linear infinite;
+        margin: auto;
+      }
+
+      @keyframes spin {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    `}</style>
+       <Suspense fallback={  <div className="loader"></div>}>
+       </Suspense>
+    </>
+}
