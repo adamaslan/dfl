72d4f00899e64b2be1b6a025613e368d40f37f5b
Adam Aslan <adamaslan@adams-air.mynetworksettings.com>
2023-05-29 12:29:29 -0400

    no-bg

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
               src="https://res.cloudinary.com/adamaslan/image/upload/v1664330111/drinksfoodlife/raki_iaoqaw_b5103d.jpg"
             />{" "}
             <h2> On the Love of Raki and Turkish Food</h2>{" "}
-            <p>Dining at Turkish Grill in Queens</p>
+            <h3>Dining at Turkish Grill in Queens</h3>
           </div>
         </Link>
 
@@ -64,7 +64,7 @@ export default function Home() {
               src="https://res.cloudinary.com/adamaslan/image/upload/v1660751024/drinksfoodlife/grandarmypunch_e9pums.jpg"
             />{" "}
             <h2> Summer Cocktails Part 2</h2>{" "}
-            <p>Grand Army Punch by @chemixtry</p>
+            <h3>Grand Army Punch by @chemixtry</h3>
           </div>
         </Link>
 
