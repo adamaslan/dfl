a8f4bdc361bd72917d050c24038fc5e7441eb97d
Adam Aslan <adamaslan@adams-air.mynetworksettings.com>
2023-06-18 11:58:13 -0400

    photo-paiiiin

--- pages/index.js
+++ pages/index.js
@@ -92,9 +92,9 @@ export default function Home() {
           </div>
         </Link>
       </div>
-
+<div>
       <ContentCard />
-
+</div>
       <Link href="/besttacosinbk" passHref>
         <div className={styles.cards2}>
           <Image
