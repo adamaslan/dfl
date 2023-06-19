e189cc5cdefae03d7b578b26ea001c3c4cb28312
Adam Aslan <adamaslan@Adams-MacBook-Air-2.local>
2023-06-19 18:36:14 -0400

    css-restyle

--- components/ContentCard.js
+++ components/ContentCard.js
@@ -1,21 +1,16 @@
 import Link from "next/link";
-import styles from "../styles/Layout.module.css";
-import {motion} from "framer-motion";
+import styles from "../styles/ContentCard.module.css";
+// import {motion} from "framer-motion";
 
 const ContentCard = () => {
     return (
         <div className={styles.cards7}>
-            <motion.div
-                // animate={{
-                //     scale: [1, 2, 2, 1, 1],
-                //     // rotate: [0, 0, 270, 270, 0],
-                //     borderRadius: ["20%", "20%", "50%", "50%", "20%"]}}
-                >
+
             <Link href="/amaromontenegro" passHref>
                 <div>
                     <img
                         alt="Amaro"
-                        className={styles.photos}
+                        // className={styles.photos}
                         src="https://res.cloudinary.com/adamaslan/image/upload/v1654725062/drinksfoodlife/amaro-monte_omsl1h.jpg"
                     />{" "}
                     <h2> Drinking Amaro Montenegro at ZXY Gallery</h2>{" "}
@@ -27,7 +22,7 @@ const ContentCard = () => {
                 <div >
                     <img
                         alt="wine"
-                        className={styles.photos}
+                        // className={styles.photos}
                         src="https://res.cloudinary.com/adamaslan/image/upload/v1654718485/drinksfoodlife/coup-wine_pjdgdv.jpg"
                     />{" "}
                     <h2> An Awkward Quest</h2> <h3>For a Funky Natural Red</h3>
@@ -38,14 +33,15 @@ const ContentCard = () => {
                 <div >
                     <img
                         alt="Tea"
-                        className={styles.photos}
+                        // className={styles.photos}
                         src="https://res.cloudinary.com/adamaslan/image/upload/v1654718487/drinksfoodlife/teasecret1_corwi6.jpg"
                     />{" "}
                     <h2> On the Love of Loose Tea</h2>
                     <h3>Especially the MIA airport location</h3>
                 </div>
             </Link>
-</motion.div>
+
+
         </div>
     );
 };
