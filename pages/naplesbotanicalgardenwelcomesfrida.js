dd2aab18c0b93ae5218a6fce9e34eecb96611054
Reena Thomas <rtthomas@ucsc.edu>
2023-08-11 18:35:59 -0700

    initial commit

--- pages/naplesbotanicalgardenwelcomesfrida.js
+++ pages/naplesbotanicalgardenwelcomesfrida.js
@@ -1,9 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Layout.module.css";
 import ContentCard from "../components/ContentCard";
-
 import headerStyles from "../styles/Header.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -20,76 +19,69 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.container}>
-        <div className={styles.article1}>
-          <br />
-          <p className={headerStyles.description}>
-            Naples Botanical Garden Welcomes Frida and the Casa Azul
-            <br />
-            <img
-              className="photo"
-              alt="Aspen"
-              src="https://res.cloudinary.com/adamaslan/image/upload/v1681154705/drinksfoodlife/frida-diego_fi26ml.jpg"
-            />{" "}
-          </p>
-          <p>
-            The Frida Exhibit at the{" "}
-            <a href="https://www.naplesgarden.org/">
-              <u>Naples Botanical Garden</u>
-            </a>{" "}
-            is a breathtaking display of Mexican culture, art, and nature. The
-            exhibit showcases replicas from Frida Kahlo's Casa Azul, her garden,
-            and the plants of Mexico, all of which were central to Kahlo's life
-            and art. Frida Kahlo was a renowned Mexican artist who gained
-            international recognition for her unique style and portrayal of
-            Mexican culture. The Casa Azul, where she was born and raised, is
-            now a museum in Mexico City that houses her personal belongings,
-            artwork, and a collection of Mexican folk art.
-          </p>{" "}
-          <br />
-          <p>
-            {" "}
-            The Naples Botanical Garden's Frida Exhibit features replicas of
-            some of Kahlo's most iconic works, including her self-portraits, her
-            famous portraits of Diego Rivera, and her paintings of Mexican
-            culture and nature. The exhibit also includes replicas of Kahlo's
-            personal belongings, such as her traditional Mexican dresses, her
-            jewelry, and her household items. One of the most impressive aspects
-            of the exhibit is the recreation of Kahlo's garden. The garden was a
-            vital part of Kahlo's life, and she spent countless hours tending to
-            it, planting new species of plants and flowers and creating
-            beautiful arrangements. The garden was also a source of inspiration
-            for many of her paintings, including her famous painting, "The Two
-            Fridas."
-          </p>
-          <p>
-            The Naples Botanical Garden's replica of Kahlo's garden includes a
-            variety of plants and flowers that were native to Mexico, such as
-            marigolds, dahlias, and Mexican sunflowers. The garden is arranged
-            in a way that mirrors the layout of Kahlo's original garden, with
-            paths lined with stones and pots filled with brightly colored
-            flowers. The exhibit also features a collection of plants from
-            Mexico that were important to Kahlo, including the agave plant,
-            which is used to make tequila, and the prickly pear cactus, which
-            was a staple food source for the indigenous people of Mexico. These
-            plants are not only beautiful, but they also offer visitors a
-            glimpse into the culture and history of Mexico.
-          </p>
-          <p>
-            Learn more about{" "}
-            <a href="https://www.museofridakahlo.org.mx/">
-              <u>the Casa Azul</u>
-            </a>{" "}
-            here.
-          </p>
+      <div className={styles.gridcontainer4}>
+        <div className={headerStyles.description}>
+          Naples Botanical Garden Welcomes Frida and the Casa Azul <br />
           <img
-            alt="Frida"
-            className={"photo"}
-            src="https://res.cloudinary.com/adamaslan/image/upload/v1681154705/drinksfoodlife/mexican-flowers_zxo1vw.jpg"
+            className={styles.photo}
+            alt="Aspen"
+            src="https://res.cloudinary.com/adamaslan/image/upload/v1681154705/drinksfoodlife/frida-diego_fi26ml.jpg"
           />{" "}
         </div>
+        <p>
+          The Frida Exhibit at the{" "}
+          <a href="https://www.naplesgarden.org/">
+            <u>Naples Botanical Garden</u>
+          </a>{" "}
+          is a breathtaking display of Mexican culture, art, and nature. The
+          exhibit showcases replicas from Frida Kahlo's Casa Azul, her garden,
+          and the plants of Mexico, all of which were central to Kahlo's life
+          and art. Frida Kahlo was a renowned Mexican artist who gained
+          international recognition for her unique style and portrayal of
+          Mexican culture. The Casa Azul, where she was born and raised, is
+          now a museum in Mexico City that houses her personal belongings,
+          artwork, and a collection of Mexican folk art.
+        </p>
+        <p>
+          The Naples Botanical Garden's Frida Exhibit features replicas of
+          some of Kahlo's most iconic works, including her self-portraits, her
+          famous portraits of Diego Rivera, and her paintings of Mexican
+          culture and nature. The exhibit also includes replicas of Kahlo's
+          personal belongings, such as her traditional Mexican dresses, her
+          jewelry, and her household items. One of the most impressive aspects
+          of the exhibit is the recreation of Kahlo's garden. The garden was a
+          vital part of Kahlo's life, and she spent countless hours tending to
+          it, planting new species of plants and flowers and creating
+          beautiful arrangements. The garden was also a source of inspiration
+          for many of her paintings, including her famous painting, "The Two
+          Fridas."
+        </p>
+        <p>
+          The Naples Botanical Garden's replica of Kahlo's garden includes a
+          variety of plants and flowers that were native to Mexico, such as
+          marigolds, dahlias, and Mexican sunflowers. The garden is arranged
+          in a way that mirrors the layout of Kahlo's original garden, with
+          paths lined with stones and pots filled with brightly colored
+          flowers. The exhibit also features a collection of plants from
+          Mexico that were important to Kahlo, including the agave plant,
+          which is used to make tequila, and the prickly pear cactus, which
+          was a staple food source for the indigenous people of Mexico. These
+          plants are not only beautiful, but they also offer visitors a
+          glimpse into the culture and history of Mexico.
+        </p>
+        <p>
+          Learn more about{" "}
+          <a href="https://www.museofridakahlo.org.mx/">
+            <u>the Casa Azul</u>
+          </a>{" "}
+          here.
+        </p>
+        <img
+          alt="Frida"
+          className={styles.photo}
+          src="https://res.cloudinary.com/adamaslan/image/upload/v1681154705/drinksfoodlife/mexican-flowers_zxo1vw.jpg"
+        />{" "}
       </div>
-
       <ContentCard />
     </>
   );
--- pages/tealove.js
+++ pages/tealove.js
@@ -1,9 +1,7 @@
-//
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Layout.module.css";
 import ContentCard from "../components/ContentCard";
-
 import headerStyles from "../styles/Header.module.css";
 
 export default function Home() {
@@ -23,51 +21,48 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.container}>
-        <div className={styles.article1}>
+      <div className={styles.gridcontainer4}>
+        <div className={headerStyles.description}>
+          5 Reasons Why You Should Drink Loose Teas
           <br />
-          <p className={headerStyles.description}>
-            5 Reasons Why You Should Drink Loose Teas
-            <br />
-            <img
-              className="photo"
-              alt="market"
-              src="https://res.cloudinary.com/adamaslan/image/upload/v1654718487/drinksfoodlife/teasecret1_corwi6.jpg"
-            />{" "}
-          </p>
-          <p>
-            Lots to say about the tea in its loosest form and genmaicha...and
-            Porto Rico Importing Company which dates back to 1907. Here are 5
-            reasons why loose tea is great!
-          </p>
-          <ol>
-            <li>
-              <strong>Lots of flavor:</strong> Loose tea leaves are larger and
-              less processed than tea found in bags, which means they retain
-              more of their natural oils and flavor.
-            </li>
-            <li>
-              <strong>Higher quality:</strong> Loose tea is often made from
-              better quality tea leaves compared to the broken leaves and dust
-              found in tea bags.
-            </li>
-            <li>
-              <strong>Health benefits:</strong> Loose tea contains antioxidants,
-              polyphenols, and other beneficial compounds that can help support
-              overall health and well-being.
-            </li>
-            <li>
-              <strong>Customizable:</strong> Loose tea allows for more control
-              over the strength and flavor of your tea. You can adjust the
-              amount of tea leaves and steeping time to your liking.
-            </li>
-            <li>
-              <strong>Eco-friendly:</strong> Loose tea generates less waste than
-              tea bags, which are often made with non-biodegradable materials
-              like nylon or polyester.
-            </li>
-          </ol>
+          <img
+            className={styles.photo}
+            alt="market"
+            src="https://res.cloudinary.com/adamaslan/image/upload/v1654718487/drinksfoodlife/teasecret1_corwi6.jpg"
+          />{" "}
         </div>
+        <p>
+          Lots to say about the tea in its loosest form and genmaicha...and Porto
+          Rico Importing Company which dates back to 1907. Here are 5 reasons why
+          loose tea is great!
+        </p>
+        <ol>
+          <li>
+            <strong>Lots of flavor:</strong> Loose tea leaves are larger and less
+            processed than tea found in bags, which means they retain more of their
+            natural oils and flavor.
+          </li>
+          <li>
+            <strong>Higher quality:</strong> Loose tea is often made from better
+            quality tea leaves compared to the broken leaves and dust found in tea
+            bags.
+          </li>
+          <li>
+            <strong>Health benefits:</strong> Loose tea contains antioxidants,
+            polyphenols, and other beneficial compounds that can help support
+            overall health and well-being.
+          </li>
+          <li>
+            <strong>Customizable:</strong> Loose tea allows for more control over
+            the strength and flavor of your tea. You can adjust the amount of tea
+            leaves and steeping time to your liking.
+          </li>
+          <li>
+            <strong>Eco-friendly:</strong> Loose tea generates less waste than tea
+            bags, which are often made with non-biodegradable materials like nylon
+            or polyester.
+          </li>
+        </ol>
       </div>
       <ContentCard />
       <div className={styles.smain}>
