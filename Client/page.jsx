9d469641726d834b4b701065fc2940cb1a77ad94
Adam Aslan <chillcoders@gmail.com>
2024-01-30 18:19:11 -0500

    folders-struct

---
+++ Client/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+"use client"
+const Member = () => {
+    return <div><h1>Member Client Session </h1></div>;
+};
+
+export default Member;
---
+++ Denied/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Denied = () => {
+    return <div><h1>Denied </h1></div>;
+};
+
+export default Denied;
