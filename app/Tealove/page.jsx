9224576fc010c9853342ac40fe95b5f66afdb4f7
Adam Aslan <chillcoders@gmail.com>
2024-04-05 15:10:37 -0500

    pages-complete

---
+++ app/Tealove/page.jsx
@@ -0,0 +1,73 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Layout.module.css";
+import ContentCard from "../(components)/ContentCard"; 
+import headerStyles from "../styles/Header.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>
+          On the Love of Loose Tea: 5 Reasons Why Loose Tea is Great
+        </title>
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/adamaslan/image/upload/v1667159646/drinksfoodlife/dfl-logo_vnu5xy.jpg"
+        />
+        <meta
+          name="description"
+          content="Drinks Food Life. A Bushwick based publication covering the tastiest things in NYC and the rest of the world."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.gridcontainer4}>
+        <div className={headerStyles.description}>
+          5 Reasons Why You Should Drink Loose Teas
+          <br />
+          <img
+            className={styles.photo}
+            alt="market"
+            src="https://res.cloudinary.com/adamaslan/image/upload/v1654718487/drinksfoodlife/teasecret1_corwi6.jpg"
+          />{" "}
+        </div>
+        <p>
+          Lots to say about the tea in its loosest form and genmaicha...and Porto
+          Rico Importing Company which dates back to 1907. Here are 5 reasons why
+          loose tea is great!
+        </p>
+        <ol>
+          <li>
+            <strong>Lots of flavor:</strong> Loose tea leaves are larger and less
+            processed than tea found in bags, which means they retain more of their
+            natural oils and flavor.
+          </li>
+          <li>
+            <strong>Higher quality:</strong> Loose tea is often made from better
+            quality tea leaves compared to the broken leaves and dust found in tea
+            bags.
+          </li>
+          <li>
+            <strong>Health benefits:</strong> Loose tea contains antioxidants,
+            polyphenols, and other beneficial compounds that can help support
+            overall health and well-being.
+          </li>
+          <li>
+            <strong>Customizable:</strong> Loose tea allows for more control over
+            the strength and flavor of your tea. You can adjust the amount of tea
+            leaves and steeping time to your liking.
+          </li>
+          <li>
+            <strong>Eco-friendly:</strong> Loose tea generates less waste than tea
+            bags, which are often made with non-biodegradable materials like nylon
+            or polyester.
+          </li>
+        </ol>
+      </div>
+      <ContentCard />
+      <div className={styles.smain}>
+        Contact us cause this is now the footer
+      </div>
+    </>
+  );
+}
---
+++ pages/babydaddy2.js
@@ -0,0 +1,68 @@
+    import Head from "next/head";
+    import styles from "../styles/Layout.module.css";
+    import ContentCard from "../components/ContentCard";
+    import headerStyles from "../styles/Header.module.css";
+
+    import ThreeJSReactComponent from '../components/Logo3D';
+
+    export default function Home() {
+      return (
+        <>
+          <Head>
+            <title>Baby Daddy Beer Please</title>
+            <meta
+              property="og:image"
+              content="https://res.cloudinary.com/adamaslan/image/upload/v1667159646/drinksfoodlife/dfl-logo_vnu5xy.jpg"
+            />
+            <meta
+              name="description"
+              content="Drinks Food Life. A Bushwick based publication covering the tastiest things in NYC and the rest of the world."
+            />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+        
+          <div className={styles.gridcontainer4}>
+            <div className={headerStyles.description}>
+              Baby Daddy Please <br />
+            </div>
+            <div>
+            <ThreeJSReactComponent glbFile="https://sketchfab.com/models/7b101d78699843e4b73d51f4e43c0b57.glb" />   </div>
+            <div className={styles.gridcontainer5}>
+              <img  
+                className={styles.photo}
+                alt="beer can"
+                src="/babydaddy.jpg"
+              />
+            </div>
+            
+            <p>
+              Baby Daddy is a great choice for anyone that wants a highly
+              drinkable beer that is full of flavor. With so many styles of beer
+              available it's easy to make an appreciative glance at the noticeably
+              hip design of baby daddy and then get distracted by some other beer
+              in the vast array of beers in the world. Ahh...so many tasty beer
+              fish in the sea.
+            </p>
+            <p>
+              Indeed, the anonymity of any beer on the shelf in a major city in
+              the US is akin to the modern era of dating. With so many IPAs (not
+              to mention session IPAs) on the market, it’s easy to swipe left on
+              any beer these days, but Baby Daddy seems to be having more success
+              than the rest of the flock these days.
+            </p>
+            <p>
+              The good tidings have most likely arisen because of the full-bodied
+              IPA taste with a very noticeable grapefruit kick that is still
+              incredibly easy to drink. A quality product coupled with cool
+              packaging and branding that is emblematic of Speakeasy ales and lagers
+              ensures Baby Daddy IPA will be a bevy daddies, mommies, and the
+              whole over 21-year-old family will be enjoying for a long time.
+              Cheers!
+            </p>
+          </div>
+
+          <ContentCard />
+        </>
+      );
+    }
+              
