80b9da09709eefb83efe062c73c2381bfe93a0d1
babyboyA <chillcoders@gmail.com>
2024-11-12 17:21:43 -0500

    no-loader

--- app/(components)/AuthProvider.js
+++ app/(components)/AuthProvider.js
@@ -1,9 +1,10 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+// import { SessionProvider } from "next-auth/react";
 
 const AuthProvider = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  // return <SessionProvider>{children}</SessionProvider>;
+<div>hiii</div>
 };
 
 export default AuthProvider;
