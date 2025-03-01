9a36da596aa10a27c427df761d357546f3456136
adamaslan <chillcoders@gmail.com>
2025-03-01 18:51:24 -0500

    chat-working-ok

--- src/app/(components)/Chatbot4.js
+++ src/app/(components)/Chatbot4.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function ChatApp() {
     const [messages, setMessages] = useState([]);
