85676a5ec1e8db569beb4dcdb8ebe3a82e3a1d45
adamaslan <chillcoders@gmail.com>
2025-05-17 19:16:47 -0400

    art3

--- src/app/the-most-fun-conventions/page.jsx
+++ src/app/the-most-fun-conventions/page.jsx
@@ -80,8 +80,9 @@ When & Where: June 10th - 11th, Industry City, Brooklyn, NY</h2>
         </p>
       <p>Whether you're a seasoned industry professional or a passionate enthusiast, these conventions offer unparalleled opportunities to taste, learn, and connect with the vibrant world of spirits and cocktail culture.</p>
 
+
       <h2>RAW WINE</h2>
-      <p>Taking place in cities around the world, including London, Berlin, New York, Los Angeles, Montréal, Toronto, Paris, Copenhagen and Tokyo, the fairs are a physical manifestation (IRL, as they say) of the RAW WINE Community. They are a great opportunity to meet some of the wine world's most extraordinary wine heroes and taste their highly individual creations – wines that are pure, kind to the planet, (probably) better for your health and, best of all, absolutely delicious.</p>
+      <p>Held in major cities globally, including London, Berlin, New York, Los Angeles, Montréal, Toronto, Paris, Copenhagen, and Tokyo, the RAW WINE fairs serve as a tangible gathering point for the RAW WINE Community. These events offer attendees a unique opportunity to connect with some of the wine world's most notable producers and sample their distinctive creations – wines celebrated for their purity, environmental consciousness, potential health benefits, and exceptional taste.</p>
       <p>
         For more on RAW WINE click <a href="https://www.rawwine.com/fairs"><u>here</u></a>
       </p>
